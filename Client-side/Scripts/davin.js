4c294ffe43a4aa850555ec183cca696963063369
Mohzis1 <isangmoses0@gmail.com>
2023-06-28 08:14:04 +0100

    yet to be modified

--- Client-side/Scripts/davin.js
+++
@@ -1,6 +0,0 @@
-clientSignup = document.getElementById("clientSignup");
-formContainer = document.getElementById("formContainer");
-
-clientSignup.addEventListener("click", function() {
-  formContainer.classList.toggle("hidden");
-});
--- Client-side/Scripts/testing.js
+++
@@ -1,8 +0,0 @@
-//document.getElementById("button").addEventListener("onclick", function() {
-    // const form = getElementById("formM");
-   //// if (form.style.display === "none") {
-      //  form.style.display = "block";
-   // } else {
-        //form.style.display = "non";
-    //}
-    });
